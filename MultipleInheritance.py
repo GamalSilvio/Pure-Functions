228faf8d160bbde666a4df89736f4811d225f28c
Gamal Silvio Gullifa <gamalgs@fb.com>
2025-01-22 16:34:48 +0000

    first commit

---
+++ MultipleInheritance.py
@@ -0,0 +1,30 @@
+class User():
+    def sign_in(self):
+        print('Logged in')
+
+class Wizard(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f'attacking with power of {self.power}')
+
+class Archer(User):
+    def __init__(self, name, arrows):
+        self.name = name
+        self.arrows = arrows
+
+    def check_arrows(self):
+        print(f'{self.arrows} remaining')
+
+    def runt(self):
+        print('Runs very fast')
+
+class HybridBorg(Wizard, Archer):
+    def __init__(self, name, power, arrows):
+        Archer.__init__(self, name, arrows)
+        Wizard.__init__(self, name, power)
+
+hb1 = HybridBorg('borgie', 100, 40)
+print(hb1.sign_in())
